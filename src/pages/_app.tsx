185f05106aa294185a44140001f5e04a9ed5efee
9utty <leegh4250@gmail.com>
2023-08-12 18:27:21 +0900

    🌈 수정: sitemap 수정(테스트)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,16 +12,6 @@ export default function App({
   return (
     <>
       <ThemeProvider theme={original}>
-        <Head>
-          <meta charSet="utf-8" />
-          <title>구티의 블로그</title>
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="https://user-images.githubusercontent.com/86397600/236520751-cbe5955c-0ec5-46d8-bc42-130ef3c62a1f.png"
-          />
-        </Head>
         <div style={{ height: "100vh" }}>
           <Component {...pageProps} />
           <Layout />
